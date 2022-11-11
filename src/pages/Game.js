9a19807e764556ad7004ba13dbc3d9635abd4cb2
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-11-11 10:16:46 +1000

    Req-6-iníciando o requisito

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,11 +1,81 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { actionCreator, getQuestions, SAVE_TOKEN } from '../redux/actions';
 
 class Game extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+    const { dispatch, history } = this.props;
+    const questionsList = dispatch(getQuestions());
+    if (questionsList.length === 0) {
+      localStorage.removeItem('token');
+      dispatch(actionCreator(SAVE_TOKEN, ''));
+      history.push('/login');
+    }
+  }
+
   render() {
+    const { questions, loading } = this.props;
+
     return (
-      <div>Game Start!</div>
+
+      !loading
+      && (
+        questions.map((question, index) => {
+          const allAnswers = [...question.incorrect_answers, question.correct_answer];
+          const randomHelperNumb = 0.5;
+          const shuffle = (arr) => [...arr].sort(() => Math.random() - randomHelperNumb);
+          const random = shuffle(allAnswers);
+          return (
+            <div key={ index }>
+              <div data-test-id="question-category">
+                {question.category}
+              </div>
+              <div>
+                <div data-test-id="question-text">
+                  {question.question}
+
+                </div>
+                <div data-test-id="answer-options">
+                  {random.map((ans, indexAnswer) => (
+                    <button
+                      type="button"
+                      key={ indexAnswer }
+                      data-test-id={ ans === question.correct_answer
+                        ? 'correct-answer' : `wrong-answer-${indexAnswer}` }
+                    >
+                      {ans}
+                    </button>
+                  ))}
+                </div>
+              </div>
+            </div>
+          );
+        })
+      )
     );
   }
 }
 
-export default Game;
+const mapStateToProps = (state) => ({
+  questions: state.game.questions,
+  loading: state.game.loading,
+
+});
+
+Game.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  dispatch: PropTypes.func.isRequired,
+  questions: PropTypes.arrayOf.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+export default connect(mapStateToProps)(Game);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,6 @@
 export const REQUEST_TOKEN = 'REQUEST_TOKEN';
 export const SAVE_TOKEN = 'SAVE_TOKEN';
+export const SAVE_QUESTIONS = 'SAVE_QUESTIONS';
 
 export const actionCreator = (type, payload) => ({
   type,
@@ -15,5 +16,19 @@ export function getToken() {
     dispatch(actionCreator(SAVE_TOKEN, tokenData.token));
     dispatch(actionCreator(REQUEST_TOKEN, false));
     localStorage.setItem('token', tokenData.token);
+  };
+}
+
+export function getQuestions() {
+  return async (dispatch) => {
+    dispatch(actionCreator(REQUEST_TOKEN, true));
+    const token = localStorage.getItem('token');
+    const url = `https://opentdb.com/api.php?amount=5&token=${token}`;
+    const response = await fetch(url);
+    const questionsData = await response.json();
+    const questionsList = questionsData.results;
+    dispatch(actionCreator(SAVE_QUESTIONS, questionsList));
+    dispatch(actionCreator(REQUEST_TOKEN, false));
+    return questionsList;
   };
 }
