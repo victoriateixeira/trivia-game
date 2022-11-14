28d619fce5cffc59e412a93bab812ed8f7bf48b4
Mirian Carla <mirianmodestofreitas@gmail.com>
2022-11-14 09:17:14 -0300

    req-19-Criado ranking

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
+import md5 from 'crypto-js/md5';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends Component {
-  constructor() {
-    super();
-    this.state = {
-      img: '',
-      name: '',
-      score: 0,
-    };
-  }
-
   render() {
-    const { img, name, score } = this.state;
+    const { email, name } = this.props;
+    const gravatar = md5(email).toString();
+    const url = `https://www.gravatar.com/avatar/${gravatar}`;
     return (
       <div>
-        <img src={ img } alt={ name } data-testid="header-profile-picture" />
+        <img src={ url } alt={ name } data-testid="header-profile-picture" />
         <p data-testid="header-player-name">{ name }</p>
-        <p data-testid="header-score">{ score }</p>
+        <p data-testid="header-score">0</p>
       </div>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.login.email,
+  name: state.login.name,
+});
+
+export default connect(mapStateToProps)(Header);
---
+++ src/pages/Ranking.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Ranking extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ranking: [],
+    };
+  }
+
+  componentDidMount() {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
+    const orderedRanking = ranking.sort((a, b) => b.score - a.score);
+    this.setState({
+      ranking: orderedRanking,
+    });
+  }
+
+  redirectToLogin = () => {
+    const { history } = this.props;
+    localStorage.removeItem('token');
+    history.push('/');
+  };
+
+  render() {
+    const { ranking } = this.state;
+    return (
+      <div>
+        <div>
+          <p>Ranking</p>
+          <button
+            type="button"
+            data-testid="btn-go-home"
+            onClick={ this.redirectToLogin }
+          >
+            Login
+          </button>
+        </div>
+        <div>
+          {ranking.map((player, i) => (
+            <div key={ i }>
+              <img src={ player.gravatarImg } alt={ player.name } />
+              <div data-testid={ `player-name-${i}` }>
+                {player.name}
+              </div>
+              <div data-testid={ `player-score-${i}` }>
+                {player.score}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+    );
+  }
+}
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default Ranking;
