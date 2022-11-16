5170f0e8a75771b2de118c4be343ebc8e9f04d75
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-11-17 05:57:31 +1000

    req-16-finalizado

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from './pages/Login';
 import Settings from './pages/Settings';
 import Game from './pages/Game';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
           <Route path="/game" component={ Game } />
           <Route path="/settings" component={ Settings } />
           <Route path="/feedback" component={ Feedback } />
+          <Route path="/ranking" component={ Ranking } />
         </Switch>
       </header>
     </div>
--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -5,57 +5,55 @@ import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
-    const { assertions, score } = this.props;
+    const { assertions, score, history } = this.props;
     const tres = 3;
 
-    if (assertions < tres) {
-      return (
-        <>
-          <Header />
+    return (
+      <>
+        <Header />
+        {assertions < tres ? <h2 data-testid="feedback-text">Could be better...</h2>
+          : <h2 data-testid="feedback-text">Well Done!</h2> }
+        <br />
+        <span>
+          You got
+          {' '}
+          <span data-testid="feedback-total-question">{assertions}</span>
+          {' '}
+          questions right!
+        </span>
 
-          <br />
+        <br />
+        <span>
+          Score:
+          {' '}
+          <span data-testid="feedback-total-score">{score}</span>
+        </span>
 
-          <h2 data-testid="feedback-text">Could be better...</h2>
-
-          <br />
-
-          <p data-testid="feedback-total-question">{assertions}</p>
-
-          <br />
-
-          <p data-testid="feedback-total-score">{score}</p>
-
-        </>
-      );
-    }
-
-    if (assertions >= tres) {
-      return (
-        <>
-
-          <Header />
-
-          <br />
-
-          <h2 data-testid="feedback-text">Well Done!</h2>
-
-          <br />
-
-          <p data-testid="feedback-total-question">{assertions}</p>
-
-          <br />
-
-          <p data-testid="feedback-total-score">{score}</p>
-
-        </>
-      );
-    }
+        <button
+          type="button"
+          onClick={ () => history.push('/') }
+          data-testid="btn-play-again"
+        >
+          Play Again
+        </button>
+        <button
+          type="button"
+          onClick={ () => history.push('/ranking') }
+          data-testid="btn-ranking"
+        >
+          Ranking
+        </button>
+      </>
+    );
   }
 }
 
 Feedback.propTypes = {
   score: PropTypes.number.isRequired,
   assertions: PropTypes.number.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
 };
 
 const mapStateToProps = (globalState) => ({
